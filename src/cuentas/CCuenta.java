21e33f5b61e0b0a9949c539a2ffc272b846ee0df
Alba Alvarez <a22albaaa@iessanclemente.net>
2023-03-09 00:59:28 +0100

    Repositorio commiteado

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -11,81 +11,98 @@ package cuentas;
  public class CCuenta {
 
     /**
-     * @return the nombre
+     * Devuelve el nombre del titular de la cuenta
+     * @return nombre titular
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta
+     * @param nombre nombre del titular de la cuenta
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Devuelve el número de cuenta
+     * @return número de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Establece el numero de cuenta
+     * @param cuenta nuemro de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Devuelve el saldo actualizado de la cuenta
+     * @return saldo actual
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Establece el saldo actual de la cuenta
+     * @param saldo saldo actual
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Devuelve el tipo de interés de la cuenta
+     * @return el tipo Interés de la cuenta
      */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
     /**
-     * @param tipoInteres the tipoInteres to set
+     * Establece el tipo de interés que se le aplicará a la cuenta
+     * @param tipoInteres tipo de interés aplicado a la cuenta
      */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
 
     /**
-     * atributos de la clase CCuenta
+     *  Nombre del titular de la cuenta
      */
     private String nombre;
+    /**
+     * Número de cuenta
+     */
     private String cuenta;
+    /**
+     * Saldo de la cuenta
+     */
     private double saldo;
+    /**
+     * Tipo de interés aplicado a la cuenta
+     */
     private double tipoInteres;
 
     /**
-     * Constructor por defecto sin parametros
+     * Constructor por defecto sin parametros con valores por defecto
      */
     public CCuenta()
     {
     }
     /**
-     * Constructor con parametros
-     * @param nom
-     * @param cue
-     * @param sal
-     * @param tipo 
+     * Constructor con parametros con valores dados
+     * @param nom  nombre del titular de la cuenta
+     * @param cue número de cuenta
+     * @param sal saldo inicial de la cuenta
+     * @param tipo tipo de interés aplicado a la cuenta
      */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
@@ -93,18 +110,31 @@ package cuentas;
         cuenta=cue;
         saldo=sal;
     }
-
+    /**
+     * Devuelve el estado actual de la cuenta
+     * @return estado actual de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Realiza un ingreso
+     * @param cantidad cantidad a ingresar en la cuenta
+     * @throws Exception si la cantidad a ingresar menor de 0
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
+    /**
+     * Realiza una retirada de saldo
+     * @param cantidad cantidad a retirar
+     * @throws Exception si la cantidad es menor o igual a 0,o si el saldo actualizado de la cuenta es menor que la cantidad a retirar 
+     */
 
     public void retirar(double cantidad) throws Exception
     {
